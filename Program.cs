9c4435f7c14bdcb182ab78351c3dced1d1bdab35
lkutsall <lkutsall@hotmail.com>
2021-03-06 18:23:30 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BackendDemoForAGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
